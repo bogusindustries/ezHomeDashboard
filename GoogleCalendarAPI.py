3568f932c353c295de368306423f06d0e9d53eed
John Zilka <johnzilka@gmail.com>
2024-10-24 23:07:03 -0500

    Collect calendar events into today and future dictionaries

--- GoogleCalendarAPI.py
+++ GoogleCalendarAPI.py
@@ -17,8 +17,7 @@ class GoogleCalenderAPI:
         self.todayEvents = []
         self.futureEvents = []
         self.localTimezone = get_localzone()
-        godDamnTimeZone = pytz.timezone(self.localTimezone)
-        self.todaysFuckingDate = datetime.now(godDamnTimeZone)
+        self.todaysFuckingDate = datetime.now(self.localTimezone).date()
         self.scopes = ["https://www.googleapis.com/auth/calendar.readonly"]
         self.creds = None
         # The file token.json stores the user's access and refresh tokens, and is
@@ -63,66 +62,19 @@ class GoogleCalenderAPI:
                 print("No upcoming events found.")
                 return
 
-        # Get the local time zone
-            #self.local_tz = get_localzone()
-
             for event in events:
                 summary = event.get('summary', 'No summary available')
-                #date = event['start'].get('date')#recurring events
-                #eventDate = "No Date"
-                #startTime = event['start'].get('dateTime')#one time events
-                #eventTime = "No Time"
                 location = event.get('location', 'No location specified')
-                #print(f"{summary}\n")
-                # if date:
-                #     if date == now.date():
-                #         self.todayEvents.append(event)
-                #     else:
-                #         self.futureEvents.append(event)
                 if "dateTime" in event["start"]:#single event
-                    #process horrible date formatting
-                    #eventDate = self.processDate(event["start"]["dateTime"])
                     dateTimeString = self.processDateTime(summary, event["start"]["dateTime"], location)
                     
                 
-                elif "date" in event["start"]:#recurring events - format doesn't make you hate google even more
-                    #process not so horrible formatting
-                    #eventDate = self.processDate(event["start"]["date"])
-                    #eventTime = "All Day"
+                elif "date" in event["start"]:#recurring events
                     dateString = self.processDate(summary, event["start"]["date"], location)
-
-
-              
-                # if startTime:
-                #     try:
-                #         # Parse the startTime as a naive datetime and assume it's in UTC
-                #         event_time_utc = datetime.fromisoformat(startTime)
-                #         # Convert to local time
-                #         local_time = event_time_utc.astimezone(local_tz)
-                #         formattedTime = local_time.strftime("%I:%M %p")
-                #         formattedDate = local_time.strftime("%Y-%m-%d")
-                #         if formattedDate == now.date():
-                #             self.todayEvents.append(event)
-                #         else:
-                #             self.futureEvents.append(event)
-                #     except ValueError as e:
-                #         print(f"Error parsing time: {e}")
-                #         formattedTime = "none"
-                #         formattedDate = "none"
-                # else:
-                #     formattedTime = "none"
-                #     formattedDate = "none"
-
-          
 
         except HttpError as error:
             print(f"An error occurred: {error}")
 
-        #seriously what fucking day is it here?
-        
-        print(f"Todays fucking date : {self.todaysFuckingDate}")
-        print(f"Time Zone: {self.localTimezone}")
-        print(f"Today now is : {self.now.date()}")
         print(f"Today's events: {self.todayEvents}")
         print(f"Future Events: {self.futureEvents}")
 
@@ -131,36 +83,21 @@ class GoogleCalenderAPI:
         local_time = event_time_utc.astimezone(self.localTimezone)
         formattedTime = local_time.strftime("%I:%M %p")
         formattedDate = local_time.strftime("%m-%d-%Y")
-        checkDate = local_time.strftime("%Y-%m-%d")
-        #print(f"Summary: {summary}")
-        #print(f"Date: {formattedDate}")
-        #print(f"DateTime: {dateTime}")
-        #print(f"Time: {formattedTime}")
-        #print(f"location: {location}\n")
-        print(f"event check date is : {checkDate}")
+        checkDate = local_time.date()
         tempDict = {
             "summary" : summary,
             "date" : formattedDate,
             "time" : formattedTime,
             "location" : location
         }
-        #if checkDate == self.now.date():
+        
         if checkDate == self.todaysFuckingDate:
             self.todayEvents.append(tempDict)
         else:
             self.futureEvents.append(tempDict)
 
-
     def processDate(self, summary, date, location):
-        #print(f"Summary: {summary}")
-        #print(f"Date is : {date}")
-        #print("Time: All Day")
-        #print(f"Location: {location}\n")
-        # if date == self.now.date():
-        #     print("event is today")
-        # else:
-        #     print("event is not today")
-
+        checkDate = datetime.strptime(date, "%Y-%m-%d").date()
         tempDict = {
             "summary" : summary,
             "date" : date,
@@ -168,14 +105,10 @@ class GoogleCalenderAPI:
             "location" : location
         }
 
-        #if date == self.now.date():
-        if date == self.todaysFuckingDate:
+        if checkDate == self.todaysFuckingDate:
             self.todayEvents.append(tempDict)
         else:
             self.futureEvents.append(tempDict)
-
-
-
 
 # calender = GoogleCalenderAPI()
 # print(calender.todayEvents)
