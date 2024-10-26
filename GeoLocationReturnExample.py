04cdffd020268a18e1f9dd6df59843c8a3369c83
John Zilka <johnzilka@gmail.com>
2024-10-25 21:46:04 -0500

    add git ignore

---
+++ GeoLocationReturnExample.py
@@ -0,0 +1,48 @@
+[
+    {
+        'name': 'Minneapolis', 
+        'local_names': {
+            'ar': 'منيابولس', 
+            'oj': 'Gakaabikaang', 
+            'en': 'Minneapolis', 
+            'pt': 'Mineápolis', 
+            'zh': '明尼阿波利斯', 
+            'hi': 'मिन्यापोलिस्', 
+            'ja': 'ミネアポリス', 
+            'oc': 'Minneapòlis', 
+            'kn': 'ಮಿನ್ಯಾಪೋಲಿಸ್', 
+            'ru': 'Миннеаполис', 
+            'uk': 'Міннеаполіс', 
+            'fa': 'مینیاپولیس', 
+            'he': 'מיניאפוליס', 
+            'eo': 'Mineapolo'
+        }, 
+        'lat': 44.9772995, 
+        'lon': -93.2654692, 
+        'country': 'US', 
+        'state': 'Minnesota'
+    }, 
+    {
+        'name': 'Minneapolis', 
+        'lat': 39.121945, 
+        'lon': -97.706703, 
+        'country': 'US', 
+        'state': 'Kansas'
+    }, 
+    {
+        'name': 'Minneapolis', 
+        'lat': 36.0993683, 
+        'lon': -81.9873491, 
+        'country': 'US', 
+        'state': 'North Carolina'
+    }
+]
+
+# by zip
+{
+  "zip": "90210",
+  "name": "Beverly Hills",
+  "lat": 34.0901,
+  "lon": -118.4065,
+  "country": "US"
+}
